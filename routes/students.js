fa68c0711ce15b77425c83a8ca75bdbc02e3bc6c
Charles Loehle <clguitar1@gmail.com>
2020-06-16 16:55:06 -0400

    finished student route front end

--- routes/students.js
+++ routes/students.js
@@ -44,36 +44,46 @@ router.get('/:id', auth, async (req, res) => {
 // @route    POST api/students
 // @desc     Add new student
 // @access   Private
-router.post('/', async (req, res) => {
-  // get front end form input values
-  const {
-    name,
-    parentName,
-    email,
-    alternateEmail,
-    phone,
-    instrument,
-  } = req.body;
+router.post(
+  '/',
+  [auth, [check('name', 'Name is required').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
 
-  try {
-    const newStudent = await new Student({
+    // get front end form input values
+    const {
       name,
       parentName,
       email,
       alternateEmail,
       phone,
       instrument,
-      user: req.user.id,
-    });
+    } = req.body;
 
-    const student = await newStudent.save();
-    // send the new student data to the front end
-    res.json(student);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server error');
+    try {
+      const newStudent = new Student({
+        name,
+        parentName,
+        email,
+        alternateEmail,
+        phone,
+        instrument,
+        user: req.user.id,
+      });
+
+      const student = await newStudent.save();
+
+      // send the new student data to the front end
+      res.json(student);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
-});
+);
 
 // @route    PUT api/students/:id
 // @desc     Update student
